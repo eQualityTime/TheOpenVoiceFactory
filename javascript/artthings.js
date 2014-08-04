d84fafcfbff5659526c12e9c6900f19f0c3aa802
joereddington <joe@joereddington.com>
2014-08-04 16:55:22 +0100

    moved things

---
+++ javascript/artthings.js
@@ -0,0 +1,35 @@
+
+function artthings(){
+reset();
+	keys[0][0]="yes";
+	keys[1][0]="";
+	keys[2][0]="";
+	keys[3][0]="";
+	keys[4][0]="no";
+
+	links[0][1]="1";
+	keys[1][1]="art";
+	keys[2][1]="paint";
+	keys[3][1]="glitter";
+	keys[4][1]="sponge";
+
+	keys[0][2]="tape";
+	keys[1][2]="eraser";
+	keys[2][2]="glue";
+	keys[3][2]="paintbrush";
+	keys[4][2]="paper";
+
+	keys[0][3]="pen";
+	keys[1][3]="pencil";
+	keys[2][3]="scissors";
+	keys[3][3]="wrapping paper";
+	keys[4][3]="";
+
+	keys[0][4]=""; 
+	keys[1][4]="";
+	keys[2][4]="";
+	keys[3][4]="";
+	keys[4][4]="materials";
+
+	document.main.src="images/artthings.png";
+}
