92968ee142de0147961ee57172ffde27a32aea51
joereddington <joe@joereddington.com>
2014-10-01 15:09:51 +0100

    adding the files...

---
+++ javascript/home.js
@@ -0,0 +1,35 @@
+
+function home(){
+reset();
+	utterances[0][0]="yes";
+	utterances[1][0]="";
+	utterances[2][0]="";
+	utterances[3][0]="";
+	utterances[4][0]="no";
+
+	links[0][1]="1";
+	utterances[1][1]="home";
+	utterances[2][1]="cellar";
+	utterances[3][1]="dining room";
+	utterances[4][1]="hall";
+
+	utterances[0][2]="attic";
+	utterances[1][2]="bathroom";
+	utterances[2][2]="lounge";
+	utterances[3][2]="study";
+	utterances[4][2]="bedroom";
+
+	utterances[0][3]="kitchen";
+	utterances[1][3]="my room";
+	utterances[2][3]="utility room";
+	utterances[3][3]="play room";
+	utterances[4][3]="";
+
+	utterances[0][4]="up stairs"; 
+	utterances[1][4]="down staris";
+	utterances[2][4]="stairs";
+	utterances[3][4]="office";
+	utterances[4][4]="";
+
+	document.main.src="images/home.jpg";
+}
---
+++ javascript/yetmoreaction.js
@@ -0,0 +1,35 @@
+
+function yetmoreaction(){
+reset();
+	utterances[0][0]="yes";
+	utterances[1][0]="";
+	utterances[2][0]="";
+	utterances[3][0]="";
+	utterances[4][0]="no";
+
+	links[0][1]="1";
+	utterances[1][1]="are";
+	utterances[2][1]="buy";
+	utterances[3][1]="can";
+	utterances[4][1]="count";
+
+	utterances[0][2]="find";
+	utterances[1][2]="hear";
+	utterances[2][2]="hold";
+	utterances[3][2]="let";
+	utterances[4][2]="live";
+
+	utterances[0][3]="ride";
+	utterances[1][3]="say";
+	utterances[2][3]="share";
+	utterances[3][3]="walk";
+	utterances[4][3]="was";
+
+	utterances[0][4]="wash"; 
+	utterances[1][4]="write";
+	utterances[2][4]="break";
+	utterances[3][4]="ask";
+	links[4][4]="action";
+
+	document.main.src="images/yetmoreaction.jpg";
+}
