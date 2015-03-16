69096e17df07cb6f205d37695b77a3581beeda5f
Joseph Reddington <josephreddington@Josephs-MacBook-Pro.local>
2015-03-16 13:16:15 +0000

    added recognistion of the link shapes

--- grab_text.py
+++ grab_text.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python
 "Extracting Utterances from CommuniKate pagesets designed in PowerPoint"
 from pptx import Presentation
+from pptx.enum.shapes import MSO_SHAPE_TYPE
 import operator
 
 column={152400:0, 1503659:1, 1600200:1, 2861846:2, 2819400:2, 2854919:2, 2854925:2, 4170660:3, 4191000:3, 5542260:4,5769114:4, 5562600:4, 5769125:4}
 row={0:0, 152400:0, 152401:0, 1981200:1, 3771900:2, 5562600:3, 5610125:3, 6095999:3, 7314625:4, 7340121:4, 7340600:4}
 #something odd about the zero, look into that. 
 
+def get_row(in_num):
+            #from http://stackoverflow.com/a/7934624/170243 
+    return row[in_num] if in_num in row else row[min(row.keys(), key=lambda k: abs(k-in_num))] 
+
+
+def get_column(in_num):
+            #from http://stackoverflow.com/a/7934624/170243 
+    return column[in_num] if in_num in column else column[min(column.keys(), key=lambda k: abs(k-in_num))] 
 
 class utterance(object):
    "recording the utterance and where it is on the screen, for now we are doing the grid, later we will allow this to use different heigh/width and placement options"
@@ -30,10 +39,18 @@ prs = Presentation("testSuite/test1/CommuniKate20-es.pptx")
 for slide in prs.slides:
     utterance_list = []
     print "new slide"
+    print dir(slide.shapes)
+    print "xxxxxx"
+    #print dir(slide.shapes.title)
+    #title_placeholder = slide.shapes.title
+    #print title_placeholder.text
+    print "New Slide!"
+    utterances = [["link" for x in range(5)] for x in range(5)] 
+    links = [["blank" for x in range(5)] for x in range(5)] 
     for shape in slide.shapes:
-        if shape.shape_type==1:
-            shape.auto_shape_type
-           
+        if shape.shape_type==MSO_SHAPE_TYPE.AUTO_SHAPE:
+            if shape.auto_shape_type==16: #foldedcorner
+               links[get_column(shape.top)][get_row(shape.left)]="real"
         if not shape.has_text_frame:
            
             continue
@@ -43,13 +60,21 @@ for slide in prs.slides:
 		text+=run.text.encode('ascii', 'ignore')
         if(text!=""):
             #add the if shape_type is text box
-            co=column[shape.top] if shape.top in column else column[min(column.keys(), key=lambda k: abs(k-shape.top))] #from http://stackoverflow.com/a/7934624/170243 
-            ro=row[shape.left] if shape.left in row else row[min(row.keys(), key=lambda k: abs(k-shape.left))] #from http://stackoverflow.com/a/7934624/170243 
- 	    utterance_list.append( utterance(co, ro, text))
-    utterance_list.sort(key=operator.attrgetter('row'))
-    utterance_list.sort(key=operator.attrgetter('column'))
-    for x in utterance_list:
-        print x
+            co=get_column(shape.top)
+            ro=get_row(shape.left)
+ 	    utterances[co][ro]=text
+    for x in range(5):
+        for y in range (5):
+            print links[x][y]
+    for x in range(5):
+        for y in range (5):
+            if links[x][y]=="real":
+                print "     links[%d][%d]=\"%s\";" % (x,y,utterances[x][y]) 
+            else: 
+                if links[x][y]=="blank": 
+                    print "utterances[%d][%d]=\"%s\";" % (x,y,utterances[x][y]) 
+                else: 
+                    raise ValueError("You never listen.")
     break
 
 
