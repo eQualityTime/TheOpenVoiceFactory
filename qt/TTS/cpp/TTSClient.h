2571131c107a564ca2f45e6326a2da24ce9bb02f
Kirsty McNaught <kirsty@dotproduct3d.com>
2015-05-03 17:21:47 +0200

    Modify TTS abstraction layer.
    We now just define an interface, so derived classes for different
    platforms can add any extra member vars or other stuff they might need. 
    Also added a factory function to create the correct derived class.
    
    We still swap in appropriate sources files in the build.

--- qt/TTS/cpp/TTSClient.h
+++ qt/TTS/cpp/TTSClient.h
@@ -2,15 +2,28 @@
 #define TTSCLIENT_H
 
 #include <QObject>
+#include <QDebug>
 
+// This defines an interface that any OS-specific TTSClient
+// must implement.
 class TTSClient: public QObject
 {
     Q_OBJECT
 public:
-    explicit TTSClient(QObject *parent = 0);
-    Q_INVOKABLE void speak(QString msg );
+    explicit TTSClient(QObject *parent = 0) :
+    QObject(parent) { }
+
+    virtual ~TTSClient() { }
+
+    Q_INVOKABLE virtual void speak(QString msg ) = 0;
 
 };
 
+// This is a factory function which must be defined on each
+// platform. It is free to create any derived TTSClient
+// appropriate to the platform.
+// So long as you pass a qobject parent to the constructor,
+// Qt will take responsibility for the allocated memory.
+TTSClient* createTTSClient(QObject *parent = 0);
 
 #endif // TTSCLIENT_H
--- qt/TTS/cpp/ios/TTSClient.cpp
+++ qt/TTS/cpp/ios/TTSClient.cpp
@@ -4,11 +4,20 @@
 
 #include <iostream>
 
-TTSClient::TTSClient(QObject *parent)
-    : QObject(parent)
-{
-}
+class IosTTSClient : public TTSClient {
 
-void TTSClient::speak(QString msg ){
-  std::cout << "Cannot speak \"" + msg.toStdString() + "\". No TTS client set up on ios yet." << std::endl;
+public:
+  explicit IosTTSClient(QObject *parent = 0):
+    TTSClient(parent)
+  {
+  }
+
+  Q_INVOKABLE virtual void speak(QString msg ) {
+    std::cout << "Cannot speak \"" + msg.toStdString() + "\". No TTS client set up on ios yet." << std::endl;
+  }
+
+};
+
+TTSClient* createTTSClient(QObject *parent) {
+  return new IosTTSClient(parent);
 }
