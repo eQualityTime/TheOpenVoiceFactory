15f00ae6c11ce01539cdb44d59cc81d5d110f254
Kirsty McNaught <kirsty_mcn@yahoo.com>
2015-05-03 13:08:46 +0200

    Speak utterances when clicked.
    Only works on Android. 
    No ‘pending’ state for utterances yet.

--- qt/main.cpp
+++ qt/main.cpp
@@ -1,11 +1,20 @@
 #include <QApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
+
+#include "TTSClient.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+
+    // Set up a top level TTS Client object, which QML can use.
+    TTSClient ttsClient(&engine);
+    engine.rootContext()->setContextProperty("TTSClient", &ttsClient);
+
+    // It's important that we register TTSClient *before* loading QML.
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
     return app.exec();
