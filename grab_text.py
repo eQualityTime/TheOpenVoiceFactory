b5f8c12bfcf62fca46f1a6d099fb6455b0b94cc4
Joe Reddington <joe@joereddington.com>
2015-08-20 13:54:25 +0100

    json file is now produced

--- grab_text.py
+++ grab_text.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python
 "Extracting Utterances from CommuniKate pagesets designed in PowerPoint"
-# Todo - make the class a relevent thing
 # Make the images export more effectively
 
 from pptx import Presentation
 from pptx.enum.shapes import MSO_SHAPE
 from pptx.enum.shapes import MSO_SHAPE_TYPE
-
+import json
 import io
 import os
 from PIL import Image
-
 
 # Note: This may not be robust to internationalisation.
 alpha = "abcdefghijklmnopqrstuvwxyz1234567890_"
@@ -122,25 +120,28 @@ class Grid:
                                         self.links[co][ro] = "real"
                                         self.colors[co][
                                             ro] = shape.fill.fore_color.rgb
-                        if not shape.has_text_frame:
-                                return
-                        text = self.utterances[co][ro]
-                        if "link" in self.utterances[co][ro]:
-                                text = ""
-                        if "Yes" in self.utterances[co][ro]:
-                                return
-                        for paragraph in shape.text_frame.paragraphs:
-                                text +="".join([run.text.encode('ascii', 'ignore') for run in paragraph.runs])
-                        if text != "":
-                                # add the if shape_type is text box
-                                self.utterances[co][ro] = text.strip()
+                        if shape.has_text_frame:
+                                self.process_text_frame(shape, co, ro)
                 except:
                         return
 
+        def process_text_frame(self, shape, co, ro):
+                text = self.utterances[co][ro]
+                if "link" in self.utterances[co][ro]:
+                        text = ""
+                if "Yes" in self.utterances[co][ro]:
+                        return
+                for paragraph in shape.text_frame.paragraphs:
+                        text += "".join([run.text.encode('ascii', 'ignore')
+                                        for run in paragraph.runs])
+                if text != "":
+                        # add the if shape_type is text box
+                        self.utterances[co][ro] = text.strip()
+
         def __str__(self):
                 body = "\n".join(
-                    [ (self.string_from_cell(row, col))
-                        for col in range (self.grid_width)
+                    [(self.string_from_cell(row, col))
+                        for col in range(self.grid_width)
                         for row in range(self.grid_width)])
                 return """
 function %s(){
@@ -163,16 +164,15 @@ document.main.src="images/CK15+.%03d.png";
                                     "You never listen.")
 
 
-def export_images(slide, utterances):
+def export_images(grid, slide):
         """     Second pass through shapes list finds images and saves them.
         We have to do this separately so it's guaranteed we already know what to
         name the images!"""
+        utterances = grid.utterances
         for shape in slide.shapes:
                 try:
-
                         if shape.shape_type == MSO_SHAPE_TYPE.PICTURE:
                                 (co, ro) = Locator.get_cr(shape.top, shape.left)
-
                                 if (co, ro) not in images:
                                         images[co, ro] = []
                                 images[co, ro].append(shape)
@@ -180,80 +180,77 @@ def export_images(slide, utterances):
                         continue
 
         # Compose each icon out of all the images in the grid cell.
-        for x in range(4):
-                for y in range(4):
-                        if (x, y) in images:
-                                # Go through all the images, compute bounding
-                                # box.
-                                l = min([shape.left for shape in images[x, y]])
-                                t = min([shape.top for shape in images[x, y]])
-                                r = max([shape.left +
-                                         shape.width for shape in images[x, y]])
-                                b = max([shape.top +
-                                         shape.height for shape in images[x, y]])
+        for (x, y) in images:
+                # Go through all the images, compute bounding
+                # box.
+                l = min([shape.left for shape in images[x, y]])
+                t = min([shape.top for shape in images[x, y]])
+                r = max([shape.left +
+                         shape.width for shape in images[x, y]])
+                b = max([shape.top +
+                         shape.height for shape in images[x, y]])
 
-                                # Scale gives us the mapping from image pixels to powerpoint
-                                # distance units. This depends on the resolution
-                                # of the images.
-                                scale = min([shape.width/shape.image.size[0]
-                                             for shape in images[x, y]])
+                # Scale gives us the mapping from image pixels to powerpoint
+                # distance units. This depends on the resolution
+                # of the images.
+                scale = min([shape.width/shape.image.size[0]
+                             for shape in images[x, y]])
 
-                                # Size of combined image, in actual pixels (not PPTX units)
-                                # If scales differ between objects, we resize
-                                # them next
-                                w = (r-l)/scale
-                                h = (b-t)/scale
-                                composite = Image.new('RGBA', (w, h))
+                # Size of combined image, in actual pixels (not PPTX units)
+                # If scales differ between objects, we resize
+                # them next
+                w = (r-l)/scale
+                h = (b-t)/scale
+                composite = Image.new('RGBA', (w, h))
 
-                                # Add all the images together.
-                                for shape in images[x, y]:
-                                        # TODO: flipping.
-                                        part = Image.open(
-                                            io.BytesIO(
-                                                shape.image.blob))
-                                        width = part.size[0]
-                                        height = part.size[1]
-                                        left = shape.crop_left*width
-                                        right = (1-shape.crop_right)*width
-                                        top = shape.crop_top*height
-                                        bottom = (1-shape.crop_bottom)*height
-                                        box = (int(left),
-                                               int(top),
-                                               int(right),
-                                               int(bottom))
-                                        part = part.crop(box)
-                                        partScale = (
-                                            shape.width /
-                                            part.size[0])  # part.size because it might have been cropped
+                # Add all the images together.
+                for shape in images[x, y]:
+                        # TODO: flipping.
+                        part = Image.open(
+                            io.BytesIO(
+                                shape.image.blob))
+                        width = part.size[0]
+                        height = part.size[1]
+                        left = shape.crop_left*width
+                        right = (1-shape.crop_right)*width
+                        top = shape.crop_top*height
+                        bottom = (1-shape.crop_bottom)*height
+                        box = (int(left),
+                               int(top),
+                               int(right),
+                               int(bottom))
+                        part = part.crop(box)
+                        partScale = (shape.width / part.size[0])
+                        # part.size because it might have been cropped
 
-                                        part = resizeImage(
-                                            part,
-                                            partScale /
-                                            scale)
+                        part = resizeImage(part, partScale / scale)
+                        composite.paste( part, ((shape.left - l)/scale,
+                             (shape.top - t)/scale),
+                            part)  # This masks out transparent pixels
 
-                                        composite.paste(
-                                            part,
-                                            ((shape.left - l)/scale,
-                                             (shape.top - t)/scale),
-                                            part)  # This masks out transparent pixels
+                # Crop final image.
+                bbox = composite.getbbox()
+                composite = composite.crop(bbox)
 
-                                # Crop final image.
-                                bbox = composite.getbbox()
-                                composite = composite.crop(bbox)
-
-                                # Save!
-                                name = remove_punctuation(
-                                    "%d-%d-" %
-                                    (x, y)+utterances[x][y]) + ".png"
-                                folder = "icons/" + str(slide_number)
-                                if not os.path.exists(folder):
-                                        os.makedirs(folder)
-                                composite.save(folder + "/" + name)
+                # Save!
+                name = remove_punctuation(
+                    "%d-%d-" %
+                    (x, y)+utterances[x][y]) + ".png"
+                folder = "icons/" + str(slide_number)
+                if not os.path.exists(folder):
+                        os.makedirs(folder)
+                composite.save(folder + "/" + name)
 
 prs = Presentation("../azulejoe/testSuite/CK15/CK15+.pptx")
 slide_number = 1
+for_json={}
 for slide in prs.slides:
-        print Grid(slide)
+        grid = Grid(slide)
+        for_json[slide_number]=[grid.tag,grid.utterances]
+#        export_images(grid, slide)
+        print grid
         slide_number += 1
 #            break
+with open('data.json', 'w') as outfile:
+    json.dump(for_json, outfile,sort_keys=True)
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
