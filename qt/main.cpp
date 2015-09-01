ad80405ad07d27a2fb0c525af6a07a0d2e60fa8c
Kirsty McNaught <kirsty_mcn@yahoo.com>
2015-09-01 20:58:02 +0200

    Fix leak of TTS client.
    Embarrassing.

--- qt/main.cpp
+++ qt/main.cpp
@@ -11,8 +11,9 @@ int main(int argc, char *argv[])
     QQmlApplicationEngine engine;
 
     // Set up a top level TTS Client object, which QML can use.    
-    TTSClient* ttsClient(createTTSClient(&engine));
-    engine.rootContext()->setContextProperty("TTSClient", ttsClient);
+    QScopedPointer<TTSClient> ttsClient;
+    ttsClient.reset(createTTSClient(&engine));
+    engine.rootContext()->setContextProperty("TTSClient", ttsClient.data());
 
     // It's important that we register TTSClient *before* loading QML.
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
