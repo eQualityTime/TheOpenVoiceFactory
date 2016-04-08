8d0c38d8b25d4a64d0452d30e4b247553b4e77fc
Joseph Reddington <joe@joereddington.com>
2016-04-08 16:35:01 +0100

    -08/04/2016-16:35:13 GMT+1: Commit http://joereddington.com/5825/2016/04/08/live-notes-azulejoe-five-by-five/
    Fixed the problem with the wrong script being called by adding a call to
    dirname.

--- upload.php
+++ upload.php
@@ -37,7 +37,7 @@ if ($uploadOk == 0) {
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded and processed, you can view the results <a href=".basename($_FILES["fileToUpload"]["name"])."/ >here</a>";
-$command = '/home/joereddington/communikate.joereddington.com/onlineprocess/create.sh '.basename($_FILES["fileToUpload"]["name"]) ;
+$command = dirname(__FILE__).'/create.sh '.basename($_FILES["fileToUpload"]["name"]) ;
 $temp = shell_exec($command );
 echo nl2br($temp);
     } else {
