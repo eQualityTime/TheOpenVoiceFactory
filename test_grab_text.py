71625065020551f5888315df998754033b9e227f
Joe Reddington <joe@joereddington.com>
2016-05-26 11:53:06 +0100

    Organising test files

--- test_grab_text.py
+++ test_grab_text.py
@@ -3,20 +3,35 @@ from pptx import Presentation
 import json
 
 
-def test_answer():
-    assert grab_text.remove_punctuation("Joe's") == "Joes"
+def test_ck12v2():
+        compare_json_files(
+            "/Users/josephreddington/Dropbox/git/azulejoe/testinputs/CK12+V2.pptx",
+            "testinputs/CK12+V2.json", 4)
 
-def test_CK20():
-    prs = Presentation("/Users/josephreddington/Dropbox/git/CommuniKate/ck20/CK20.pptx")
-    grab_text.gridSize =5
-    grids = grab_text.extract_grid(prs)
-    grids = grab_text.extract_and_label_images(prs, grids, "", False)
-    internal=grab_text.create_json_object(grids)
-    internal=json.dumps(internal)
-    grab_text.write_to_JSON(grids,"temp.json")
-    local=0
-    with open('/Users/josephreddington/Dropbox/git/azulejoe/temp.json', 'r') as f:
-        local = json.load(f)
-    with open('/Users/josephreddington/Dropbox/git/azulejoe/CK20V2.json', 'r') as f:
-        real = json.load(f)
-        assert real==local
+
+def test_ck20v2():
+        compare_json_files(
+            "/Users/josephreddington/Dropbox/git/azulejoe/testinputs/CK20V2.pptx",
+            "testinputs/CK20V2.json", 5)
+
+
+#def test_ck20v2_bg():
+#        compare_json_files(
+#            "/Users/josephreddington/Dropbox/git/azulejoe/testinputs/CK20V2-BG.pptx",
+#            "testinputs/CK20V2-BG.json", 5)
+#
+
+def compare_json_files(pres_loc, target_loc, gridSize):
+        prs = Presentation(pres_loc)
+        grab_text.gridSize = gridSize
+        grids = grab_text.extract_grid(prs)
+        grids = grab_text.extract_and_label_images(prs, grids, "", False)
+        internal = grab_text.create_json_object(grids)
+        internal = json.dumps(internal)
+        grab_text.write_to_JSON(grids, "temp.json")
+        local = 0
+        with open('/Users/josephreddington/Dropbox/git/azulejoe/temp.json', 'r') as f:
+                local = json.load(f)
+        with open(target_loc, 'r') as f:
+                real = json.load(f)
+                assert real == local
