3580ff5967ec5470547a9adc4ae4b3065c2167e5
Joseph Reddington <joe@joereddington.com>
2016-07-02 16:12:00 +0100

    fixed the message height

--- templates/javascript/read_json.js
+++ templates/javascript/read_json.js
@@ -36,7 +36,7 @@ function setup() {
     area = document.getElementById('messagewindow');
     $(area).css('width', my_width);
     $(area).css('left', (720 / grid_size_columns) + 7);
-    $(area).css('top', 112) ;
+    $(area).css('top', 98) ;
     $(area).css('height', my_height);
 }
 
