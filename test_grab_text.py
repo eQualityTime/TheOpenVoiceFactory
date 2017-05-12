43a66d1ce6a0a4b3596aa02f9af7606032541d5f
Joe Reddington <joe@joereddington.com>
2017-05-12 16:21:55 +0100

    test now passing

--- test_grab_text.py
+++ test_grab_text.py
@@ -4,6 +4,23 @@ import grab_text
 import urllib
 import json
 import os
+import sys
+sys.path.append('/home/joereddington/')
+from pptx import Presentation
+from pptx.enum.shapes import MSO_SHAPE_TYPE
+from pprint import pprint
+import json
+import io
+import os
+import math
+import string
+import unicodedata
+from PIL import Image
+from sys import argv
+from pptx.enum.shapes import MSO_SHAPE
+from pptx.enum.shapes import MSO_SHAPE_TYPE
+
+
 
 
 class ovfTest(TestCase):
@@ -15,8 +32,9 @@ class ovfTest(TestCase):
 
     def test_read_CK20_and_count_slides(self):
         prs = Presentation("testinputs/CK20V2.pptx")
-        grids = extract_grid(prs)
-        self.assertEqual(len(grids),3)
+        grab_text.gridSize=5
+        grids = grab_text.extract_grid(prs)
+        self.assertEqual(len(grids),100)
 
 if __name__=="__main__":
     unittest.main()
