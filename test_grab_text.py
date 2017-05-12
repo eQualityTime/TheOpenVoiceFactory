cda71cc2e2a44c37893f6e52f5f66e19d23330a8
Joe Reddington <joe@joereddington.com>
2017-05-12 16:19:10 +0100

    first test written

--- test_grab_text.py
+++ test_grab_text.py
@@ -1,42 +1,22 @@
+from unittest import TestCase
+import unittest
 import grab_text
-from pptx import Presentation
+import urllib
 import json
+import os
 
 
-def test_ck12v2():
-        compare_json_files(
-            "/Users/josephreddington/Dropbox/git/azulejoe/testinputs/CK12+V2.pptx",
-            "testinputs/CK12+V2.json", 4)
+class ovfTest(TestCase):
 
-def test_ck20v2():
-        compare_json_files(
-            "/Users/josephreddington/Dropbox/git/azulejoe/testinputs/CK20V2.pptx",
-            "testinputs/CK20V2.json", 5)
+    def test_first(self):
+         self.assertEqual(3,3)
 
 
-def test_ck20v2_bg():
-        compare_json_files(
-            "/Users/josephreddington/Dropbox/git/azulejoe/testinputs/CK20V2-BG.pptx",
-            "testinputs/CK20V2-BG.json", 5)
 
-def test_ck20v2ara():
-        grab_text.bordercolor =True
-        compare_json_files(
-            "/Users/josephreddington/Dropbox/git/azulejoe/testinputs/CK20V2arabic.pptx",
-            "testinputs/CK20V2arabic.json", 5)
-        grab_text.bordercolor =False
+    def test_read_CK20_and_count_slides(self):
+        prs = Presentation("testinputs/CK20V2.pptx")
+        grids = extract_grid(prs)
+        self.assertEqual(len(grids),3)
 
-def compare_json_files(pres_loc, target_loc, gridSize):
-        prs = Presentation(pres_loc)
-        grab_text.gridSize = gridSize
-        grids = grab_text.extract_grid(prs)
-        grids = grab_text.extract_and_label_images(prs, grids, "", False)
-        internal = grab_text.create_json_object(grids)
-        internal = json.dumps(internal)
-        grab_text.write_to_JSON(grids, "temp.json")
-        local = 0
-        with open('/Users/josephreddington/Dropbox/git/azulejoe/temp.json', 'r') as f:
-                local = json.load(f)
-        with open(target_loc, 'r') as f:
-                real = json.load(f)
-                assert real == local
+if __name__=="__main__":
+    unittest.main()
