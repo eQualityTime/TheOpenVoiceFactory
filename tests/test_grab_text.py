05b09ac2e49272ce3c0b57e3fd84145420f224eb
Joe Reddington <joe@joereddington.com>
2017-05-23 15:17:41 +0100

    All tests passing

---
+++ tests/test_grab_text.py
@@ -0,0 +1,123 @@
+from unittest import TestCase
+import unittest
+import mock
+import urllib
+import json
+import os
+import sys
+sys.path.append('/home/joereddington/')
+sys.path.append('..')
+import grab_text
+from pptx import Presentation
+from pptx.enum.shapes import MSO_SHAPE_TYPE
+from pprint import pprint
+import json
+import io
+import os
+import math
+import string
+import unicodedata
+from PIL import Image
+from sys import argv
+from pptx.enum.shapes import MSO_SHAPE
+from pptx.enum.shapes import MSO_SHAPE_TYPE
+
+
+
+
+class ovfTest(TestCase):
+
+    CK20=None
+
+    def get_singleton_CK20(self):
+        if not self.CK20:
+            prs = Presentation("CK20V2cutdown.pptx")
+            grab_text.gridSize=5
+            self.CK20 = grab_text.extract_grid(prs)
+        return self.CK20
+
+    def test_first(self):
+         self.assertEqual(3,3)
+
+
+
+    def test_read_CK20_and_count_slides(self):
+        grids = self.get_singleton_CK20()
+        self.assertEqual(len(grids),10)
+
+
+    def test_read_CK20_and_check_titles(self):
+        grids = self.get_singleton_CK20()
+        self.assertEqual(grids[0].tag,"Top page")
+
+
+    def test_read_CK20_and_check_color(self):
+        grids = self.get_singleton_CK20()
+        self.assertEqual(grids[0].colors[2][2],(255,125,236))
+
+    def test_read_CK20_and_check_link(self):
+        grids = self.get_singleton_CK20()
+        print "XXXXXX"
+        print grids[0].labels[4][1]
+        print grids[0].links[4][1]
+        print "XXXXXX"
+        self.assertNotEqual(grids[0].links[4][1],"")
+
+
+
+
+    def test_read_CK20_and_check_link_neg(self):
+        grids = self.get_singleton_CK20()
+        self.assertEqual(grids[0].links[1][1],"")
+
+    def test_make_title(self):
+        tag = grab_text.make_title("A sentance")
+        self.assertEqual(tag,"asentance")
+
+    def test_make_title_2(self):
+        tag = grab_text.make_title("ThInGs In MiXeD cAsE")
+        self.assertEqual(tag,"thingsinmixedcase")
+
+    def test_make_title_3(self):
+        tag = grab_text.make_title("Sysbols#")
+        self.assertEqual(tag,"sysbols")
+
+    def test_warning_for_missinglink(self):
+        grab_text.addwarning=mock.Mock(return_value=None)
+        prs = Presentation("CK20V2cutdown.pptx") #Can't use the sinlgeton because it might have been called before the mocking
+        grab_text.gridSize=5
+        grab_text.extract_grid(prs)
+        grab_text.addwarning.assert_called()
+
+    def test_regession_ck12(self):
+        regress("regression_tests_size_4/CK12+V2.pptx",4)
+
+    def test_regession_ck20_slo(self):
+        regress("regression_tests_size_5/CK20_slo_regession.pptx",5)
+
+    def test_regession_ck20v2_bg(self):
+        regress("regression_tests_size_5/CK20V2.pptx",5)
+
+
+
+def regress(filename,size):
+        compare_json_files(filename,filename+".json", size)
+
+def compare_json_files(pres_loc, target_loc, gridSize):
+        prs = Presentation(pres_loc)
+        grab_text.gridSize = gridSize
+        grids = grab_text.extract_grid(prs)
+        grids = grab_text.extract_and_label_images(prs, grids, "", False)
+        internal = grab_text.create_json_object(grids)
+        internal = json.dumps(internal)
+        grab_text.write_to_JSON(grids, "temp.json")
+        local = 0
+        with open('temp.json', 'r') as f:
+                local = json.load(f)
+        with open(target_loc, 'r') as f:
+                real = json.load(f)
+                assert real == local
+
+
+if __name__=="__main__":
+    unittest.main()
