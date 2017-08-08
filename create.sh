232c39616091266f326f4a31f148ac11aab1716d
Joseph Reddington <joe@joereddington.com>
2017-08-08 10:42:43 +0100

    minor tidy up

--- create.sh
+++ create.sh
@@ -1,9 +1,6 @@
 filelocation=$1
 size=$2
 targetname=$3
-targetlanguage=$4
-extension="${filelocation##*.}"
 mkdir $targetname
 python grab_text.py $filelocation $targetname $size
 cp -R templates/* $targetname
-perl -pi -e "s/XXX/$4/" $targetname/*.pl
