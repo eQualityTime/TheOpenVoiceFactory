06a0e2f8b9f68030e76fe0bc0dd587a5f4b3fb20
Joe Reddington <joe@joereddington.com>
2018-01-22 16:05:24 +0000

    another passing test

--- templates/javascript/tests/read_json.test.js
+++ templates/javascript/tests/read_json.test.js
@@ -20,7 +20,6 @@ describe('Comscan', function() {
 
 // # List of tests 
 // * Can you fetch an object from a json file? 
-// * Does that object have the right number of slides in it. 
 // * When loading the first page. Do a selection of the right squares have the correct message in them?
 // * When pressing a particular button, does the message window change apropreityly 
 // * When pressing a particular button, does the contents of other buttons change. 
@@ -31,15 +30,25 @@ describe('Comscan', function() {
 // That should be enought to get started.
 
 
+// * Does that object have the right number of slides in it. 
     it('We can parse an example json document', function() {
         setupInternalDataStructures(sample);	
         expect(Object.keys(links).length).toBe(100);
     });
 
+// * When loading the first page. Do a selection of the right squares have the correct message in them?
+    it('When loading top page, the utterances are in the right place', function() {
+        setupInternalDataStructures(sample);	
+        expect(labels["toppage"][0][0]).toBe('Yes');
+        expect(labels["toppage"][1][1]).toBe('I want to talk to you.');
+    });
+
+// * When pressing a particular button, does the message window change apropreityly 
+
+
+
     it('the highlight outputs -thinking- speech each time it is moved', function() {
-        iterator = testiteratorA();
         spyOn(window, "think");
-        iterator.next()
         expect(think).toHaveBeenCalled();
         //learned this at http://www.htmlgoodies.com/html5/javascript/spy-on-javascript-methods-using-the-jasmine-testing-framework.html#fbid=KJtVgELupgs
     });
