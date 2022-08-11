ee6904b9edf69b598c66dae040172983c054145a
Joe Reddington <joe@equalitytime.co.uk>
2022-08-11 05:59:28 +0000

    Fix #142
    OVF now only creates obz files. This will save space and make the code
    simpler.
    
    Also some QoL edits:
    
    * create and localcreate have been merged into create.sh which knows
      if it's being run on server.
    * create.sh now respects the python exit codes.
    * upload.php now has a seperate css file and it's php has been
          refactored
    * the local webinterface has been rewritten

--- create.sh
+++ create.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 cd "$(dirname "$0")"
+
+# Platform specific code
+designdirectory='unknown'
+unamestr=$(uname)
+if [[ "$unamestr" == 'Linux' ]]; then
+   export PATH="/usr/local/texlive/2021/bin/x86_64-linux:$PATH"
+   designdirectory="../designs/"
+elif [[ "$unamestr" == 'Darwin' ]]; then
+   designdirectory="."
+fi
+
+
 echo "Starting Script"
 filelocation=$1
 size=$2
 targetname=$3
-mkdir $targetname
-cp -R templates/* $targetname
-python3 main.py $filelocation $targetname $size
-mv $targetname ../designs.theopenvoicefactory.org/
-echo "Script Completed"
-echo $1 $2 $3 
+if python3 main.py $filelocation $targetname $size; then
+	echo "The python exited normally"
+	mv  templates/data/pageset.obz $targetname.obz
+	mv $targetname.obz $designdirectory
+	#The two lines above can clearly be combined. 
+else
+	echo "The python had an error and the script terminated"
+	echo $1 $2 $3 
+fi
+#Cleanup
+rm templates/data/images/*.png # So we don't remove the readmes.
+rm templates/data/boards/* 
+rm templates/data/*.*
--- templates/json/readme.txt
+++
@@ -1,1 +0,0 @@
-This directory should be empty - indeed, future iterations will dispense with this directory entirely. 
