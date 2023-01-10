d2ac98cef7d6c54add4f5b09195231e88adc7a66
Joe Reddington <joe.reddington@gmail.com>
2023-01-10 14:54:03 +0000

    * The vimdiff command was fixed * test_regressions now gives a list of all the files (and vimdiff   commands) that failed matches rather than stopping on the first one in a set

--- tests/regressions/test_regressions.py
+++ tests/regressions/test_regressions.py
@@ -31,46 +31,39 @@ from pptx.enum.shapes import MSO_SHAPE_TYPE
 class ovfTest(TestCase):
 
 
-    #TODO: we could speed this up by only looking at each file once - currently we completely process each pptx twice. 
 
     def test_obz_pages(self):
 
-#         generate_obz("tests/regressions/regression_tests_size_4/CK12+V2.pptx",4)
-#         generate_obz("tests/regressions/regression_tests_size_5/CK20V2.pptx",5)
-#         generate_obz("tests/regressions/regression_tests_size_5/CK20V2_ara_regession.pptx",5)
-#         generate_obz("tests/regressions/regression_tests_size_5/CK20V2_bg_regression.pptx",5)
-#         generate_obz("tests/regressions/regression_tests_size_5/CK20V2_gr_regression.pptx",5)
-#         generate_obz("tests/regressions/regression_tests_size_5/CK20V2_sk_regression.pptx",5)
-#         generate_obz("tests/regressions/regression_tests_size_5/CK20_slo_regession.pptx",5)
-#         generate_obz("tests/regressions/regression_tests_size_5/CK20V2-place.pptx",5)
-#         generate_obz("tests/regressions/regression_tests_size_5/CK20V2_es_regression.pptx",5)
-
-
         self.assertTrue(generate_obz_and_compare_to_records("tests/regressions/regression_tests_size_4/CK12+V2.pptx",4))
         self.assertTrue(generate_obz_and_compare_to_records("tests/regressions/regression_tests_size_5/CK20V2.pptx",5))
-        self.assertTrue(generate_obz_and_compare_to_records("tests/regressions/regression_tests_size_5/CK20V2_ara_regession.pptx",5))
         self.assertTrue(generate_obz_and_compare_to_records("tests/regressions/regression_tests_size_5/CK20V2_bg_regression.pptx",5))
         self.assertTrue(generate_obz_and_compare_to_records("tests/regressions/regression_tests_size_5/CK20V2_gr_regression.pptx",5))
         self.assertTrue(generate_obz_and_compare_to_records("tests/regressions/regression_tests_size_5/CK20V2_sk_regression.pptx",5))
         self.assertTrue(generate_obz_and_compare_to_records("tests/regressions/regression_tests_size_5/CK20_slo_regession.pptx",5))
         self.assertTrue(generate_obz_and_compare_to_records("tests/regressions/regression_tests_size_5/CK20V2-place.pptx",5))
         self.assertTrue(generate_obz_and_compare_to_records("tests/regressions/regression_tests_size_5/CK20V2_es_regression.pptx",5))
+        self.assertTrue(generate_obz_and_compare_to_records("tests/regressions/regression_tests_size_5/CK20V2_ara_regession.pptx",5))
     
 
 def generate_obz_and_compare_to_records(filename, page_size):
         generate_obz(filename,page_size)
         print("We are in directory {}".format(filename))
+        is_same=True # We use this boolean so we get a list of the files that fail the match rather than only the first.
         if not compare(filename+".obz.attempt/manifest.json"):
+            print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
             print("Regression Test error on file manifest")
             print("try vimdiff "+ filename+".obz.attempt/manifest.json "+ filename+".obz.target/manifest.json")
-            return False
+            print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+            is_same=False
         print("Now we start the comparisons") 
         for file in glob.glob(filename+".obz.attempt/boards/*.obf"):
             if not compare(file):
+                print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
                 print("Regression Test error on file {}".format(file))
-                print("try vimdiff {}.obz.attempt/{} {}.obz.attempt/{}".format(filename, file, filename, file))
-                return False
-        return True
+                print("try vimdiff  {} {}".format(file, file.replace('attempt','target')))
+                print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+                is_same=False
+        return is_same 
 
 def compare(file):
     target=file.replace('attempt','target')
